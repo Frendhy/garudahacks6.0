90958c5ec1311a05752c90f7548d4f7bfded5f5b
Frendhy <frendhyzhuang@gmail.com>
2025-07-25 03:49:14 +0700

    fix index bug

--- login_proses.php
+++ login_proses.php
@@ -24,7 +24,7 @@ $stmt->execute([$email]);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if ($user) {
-    if ($password == $user['password']) {
+    if (password_verify($password, $user['password'])) {
         session_start();
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['peran'] = $peran;
